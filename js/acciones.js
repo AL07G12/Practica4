b3690fd7422414d4da9bd20a027d145c73051cdb
Ruth Elizabeth <mendozaelizabeth964@gmail.com>
2014-08-04 09:40:31 -0500

    Practica4
    sonido

---
+++ js/acciones.js
@@ -0,0 +1,13 @@
+//JavaScript
+	
+$(document).ready(function(e){
+	$('#Beep').click(function(e){
+	alert('le acabas de dar click a Beep');	
+	});
+	
+	$('#Vibrar').click(function(e){
+	alert('le acabas de dar click a Vibrar');	
+	
+	
+	});
+});//ready
